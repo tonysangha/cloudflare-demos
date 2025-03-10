a24bb30312b96f2ccd4a05bdfd6b3cf51c7b37f5
Tony Sangha <tsangha@cloudflare.com>
2025-03-10 13:10:28 +1100

    2025-03-10-output-updates

--- cloudflare_one/private-dns-lb-mwan-warp/outputs.tf
+++ cloudflare_one/private-dns-lb-mwan-warp/outputs.tf
@@ -6,14 +6,15 @@ output "build_time" {
   value = formatdate("YYYY MMMM DD - hh:mm:ss ZZZ", timestamp())
 }
 
+
 output "ipsec_tunnel_ids" {
   value = {
-    for k, v in cloudflare_magic_wan_ipsec_tunnel.ipsec_rtr : k => v.id
+    for k, v in module.magic_wan : k => v.tunnel_id
   }
 }
 
 output "instance_public_ip" {
   value = {
-    for k, v in google_compute_instance.magic-wan-rtr : k => v.network_interface.0.access_config.0.nat_ip
+    for k, v in module.magic_wan_router : k => v.public_ip
   }
 }
